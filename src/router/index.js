c6dc3505be4717dfb3f56378ae647ace160c0705
jsg3121 <xodm95@gmail.com>
2020-08-28 12:57:13 +0900

    intro 화면

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "@/views/Home";
+// import Home from "@/views/Home";
+// import About from "@/views/About";
+import Intro from "@/views/Intro";
 
 Vue.use(Router);
 /* eslint-disable */
@@ -8,16 +10,20 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: "/",
+    //   name: "Home",
+    //   component: Home
+    // },
+    // {
+    //   path: "/about",
+    //   name: "About",
+    //   component: About
+    // }
     {
       path: "/",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/About.vue")
+      name: "Intro",
+      component: Intro
     }
   ]
 });
---
+++ src/views/Intro.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="intro">
+    <div class="container">
+      <div class="info_box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+#intro {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+
+  .container {
+    width: 100%;
+    height: 100%;
+    background-color: #161921;
+
+    .info_box {
+      position: absolute;
+      top: 26%;
+      right: 0;
+      width: 71.25rem;
+      height: 37.5rem;
+      background-color: #0f48ed;
+    }
+  }
+}
+</style>
