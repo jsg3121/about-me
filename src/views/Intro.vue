3978435363584a7766752d3fc72ff0e6a10142f1
jsg3121 <xodm95@gmail.com>
2020-08-28 16:56:47 +0900

    모션 추가완료

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -3,23 +3,31 @@
     <div class="container" id="container">
       <div class="info_box" id="info_box">
         <h3>들어가기전에</h3>
-        <p>이 페이지는 프로젝트나 스터디를 진행하며 배운 기술을 기록하는곳이며 지속적으로 업데이트 됩니다.</p>
-        <p>외부에서 가져온 소스나 정보는 별도의 페이지에 기록했으며 상업적인 정보는 기재하지 않았습니다.</p>
         <p>
-          IE는 11버전에서 보일 수 있도록 제작했으며 11이하의 버전에서는 화면이 제대로 표시되지 않습니다.
+          이 페이지는 프로젝트나 스터디를 진행하며 배운 기술을 기록하는곳이며
+          지속적으로 업데이트 됩니다.
+        </p>
+        <p>
+          외부에서 가져온 소스나 정보는 별도의 페이지에 기록했으며 상업적인
+          정보는 기재하지 않았습니다.
+        </p>
+        <p>
+          IE는 11버전에서 보일 수 있도록 제작했으며 11이하의 버전에서는 화면이
+          제대로 표시되지 않습니다.
           <br />크롬 브라우저에서 확인해주시면 감사하겠습니다.
         </p>
-        <p>부족한 부분이 많이 있으며 더 나은 방법과 기술로 도움을 주실 수 있으신 분들은 아래 주소로 메일을 보내주시나 git으로 방문해주시면 감사하겠습니다.</p>
         <p>
           Gmail :
           <a href="mailto:xodm95@gmail.com">xodm95@gmail.com</a>
           <br />Github :
-          <a
-            href="https://github.com/jsg3121/aboutMe"
-            target="_blank"
-          >https://github.com/jsg3121/aboutMe</a>
+          <a href="https://github.com/jsg3121/aboutMe" target="_blank"
+            >https://github.com/jsg3121/aboutMe</a
+          >
         </p>
-        <p>제작자의 허락없이 본 사이트의 내용을 다른 사이트에 무단으로 게시하는것을 금합니다.</p>
+        <p>
+          제작자의 허락없이 본 사이트의 내용을 다른 사이트에 무단으로
+          게시하는것을 금합니다.
+        </p>
         <div class="container-btn">
           <button>
             작업물 보러가기
@@ -31,12 +39,17 @@
       </div>
       <div class="title">
         <figure>
-          <img src="../assets/image/title@2x.png" alt="title" draggable="false" />
+          <img
+            id="title-1"
+            src="../assets/image/title@2x.png"
+            alt="title"
+            draggable="false"
+          />
         </figure>
-        <h1>2020</h1>
+        <h1 id="title-2">2020</h1>
       </div>
       <div class="name">
-        <h4>Jang Sun Gyu</h4>
+        <h4 id="name">Jang Sun Gyu</h4>
       </div>
     </div>
   </div>
@@ -56,10 +69,19 @@ export default {
         document.getElementById("container").classList.add("main-opening-2");
       }, 950);
       setTimeout(() => {
+        document.getElementById("title-1").classList.add("title-opening-1");
+      }, 1850);
+      setTimeout(() => {
+        document.getElementById("title-2").classList.add("title-opening-2");
+      }, 1950);
+      setTimeout(() => {
         document.getElementById("info_box").classList.add("notice-opening-1");
-      }, 1700);
-    },
-  },
+      }, 2000);
+      setTimeout(() => {
+        document.getElementById("name").classList.add("name-opening");
+      }, 3000);
+    }
+  }
 };
 </script>
 
@@ -70,29 +92,30 @@ export default {
   position: relative;
 
   .container {
-    width: 0;
-    height: 0.125rem;
+    width: 1px;
+    height: 0;
     background-color: #161921;
-    transition: 0.7s;
+    transition: width 0.7s, height 0.7s;
     position: absolute;
     left: 50%;
-    transform: translateX(-50%);
+    top: 50%;
+    transform: translate(-50%, -50%);
     overflow: hidden;
 
     .info_box {
       position: absolute;
-      top: 26%;
+      top: 14.375rem;
       right: 0;
       width: 0;
-      height: 55%;
+      height: 34rem;
       background-color: #0f48ed;
-      transition: 0.7s;
+      transition: width 0.7s;
       overflow: hidden;
       white-space: nowrap;
 
       h3 {
         color: #efefef;
-        margin-top: 8.25rem;
+        margin-top: 6.25rem;
         margin-left: 3.8125rem;
         margin-bottom: 1.8125rem;
         font-size: 2.125rem;
@@ -107,6 +130,7 @@ export default {
         font-weight: normal;
         text-align: left;
         line-height: 1.5;
+        letter-spacing: 0.0625rem;
 
         a {
           color: #efefef;
@@ -155,16 +179,21 @@ export default {
 
     .title {
       position: absolute;
-      left: 16.6%;
-      top: 19.4%;
-      width: 44%;
+      left: 20rem;
+      top: 10rem;
+      width: 100%;
+      overflow: hidden;
 
       figure {
-        height: 100%;
+        width: 52.75rem;
+        height: 8.5rem;
+        overflow: hidden;
+
         img {
-          height: 100%;
-          width: auto;
+          width: 100%;
           user-select: none;
+          transition: transform 0.5s;
+          transform: translateY(100%);
         }
       }
 
@@ -177,6 +206,16 @@ export default {
         margin-top: 0.875rem;
         user-select: none;
         width: auto;
+        transform: translateY(100%);
+        transition: transform 0.5s;
+      }
+
+      .title-opening-1 {
+        transform: translateY(0);
+      }
+
+      .title-opening-2 {
+        transform: translateY(0);
       }
     }
     .notice-opening-1 {
@@ -184,10 +223,20 @@ export default {
     }
   }
 
+  .main-opening-1 {
+    height: 100%;
+  }
+
+  .main-opening-2 {
+    width: 100%;
+  }
+
   .name {
     position: absolute;
-    left: 16.6%;
-    top: 77.4%;
+    left: 20rem;
+    top: 46.875rem;
+    overflow: hidden;
+
     h4 {
       font-family: "Montserrat";
       font-size: 1.25rem;
@@ -196,14 +245,72 @@ export default {
       text-align: left;
       color: #efefef;
       font-style: italic;
+      transition: 0.9s;
+      transform: translateY(100%);
+    }
+
+    .name-opening {
+      transform: translateY(0);
     }
   }
 }
-#intro .main-opening-1 {
-  width: 2px;
-  height: 100%;
+
+@media screen and (max-width: 1720px) {
+  #intro {
+    .container {
+      .title,
+      .name {
+        left: 15rem;
+      }
+
+      .title {
+        top: 13.125rem;
+      }
+
+      .info_box {
+        top: 17.5rem;
+        p,
+        h3 {
+          margin-left: 2.7rem;
+        }
+
+        .container-btn {
+          left: 2.7rem;
+        }
+      }
+
+      .name {
+        top: 50rem;
+      }
+    }
+  }
 }
-#intro .main-opening-2 {
-  width: 100%;
+
+@media screen and (max-width: 1440px) {
+  #intro {
+    .container {
+      .title,
+      .name {
+        left: 10rem;
+      }
+
+      .info_box {
+        top: 17.5rem;
+
+        p,
+        h3 {
+          margin-left: 2.7rem;
+        }
+
+        .container-btn {
+          left: 2.7rem;
+        }
+      }
+
+      .notice-opening-1 {
+        width: 55rem;
+      }
+    }
+  }
 }
 </style>
